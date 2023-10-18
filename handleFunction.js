97d760a1d2ed0c182516a3bb07c0640d3ee0db9b
Long LH <luonghoanglong.3008@gmail.com>
2023-10-18 12:04:58 +0700

    push file

---
+++ handleFunction.js
@@ -0,0 +1,182 @@
+
+// ----------------------------- Declare DOM -----------------------------------------
+const presskey = document.getElementById('presskey');
+const wrong = document.getElementById('wrong');
+
+const type = document.getElementById("myInput");
+const start = document.getElementById('start');
+const boxtext = document.getElementById('text');
+const text = document.querySelector('#text h3');
+var hide = document.querySelector('#text #hide').value;
+
+var clockCounting = document.querySelector('#clock span');
+
+//--------------------------- Get all keys exist on screen ---------------------------
+// Select row key on keyboard
+const row1 = document.querySelector('#rowkey1');
+const row2 = document.querySelector('#rowkey2');
+const row3 = document.querySelector('#rowkey3');
+
+// Render key on keyboard
+function renderKey(array, rowkey) {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        var key = document.createElement('p');
+        key.textContent = element.name;
+        key.classList.add('key');
+        key.dataset.keycode = element.keycode
+        rowkey.appendChild(key);
+    }
+}
+
+renderKey(rowkey1, row1)
+renderKey(rowkey2, row2)
+renderKey(rowkey3, row3)
+
+const allkeyonscreen = document.querySelectorAll('.key');
+
+// ----------------------------- Get started -----------------------------------------
+var startTime = 5;
+start.onclick = () => {
+    boxtext.classList.add('appear');
+    start.classList.add('disapear');
+    type.placeholder = "Type in here...";
+    // var interval = setInterval(() => {
+    //     getStart()
+    // }, 1000)
+
+    // setTimeout(() => {
+    //     clearInterval(interval)
+    //     startTime = 5;
+        type.disabled = false;
+        type.focus();
+        replace();
+    //     setNewTime();
+    // }, startTime * 1000)
+}
+
+// function getStart() {
+//     startTime--;
+//     clockCounting.textContent = startTime + 's';
+// }
+
+// function setNewTime() {
+//     startTime = 3;
+
+//     var interval = setInterval(() => {
+//         getStart();
+//     }, 1000)
+
+//     setTimeout(() => {
+//         clearInterval(interval)
+//         startTime = 0;
+//         type.disabled = true;
+//         type.value = 'Your result is ...'
+//         // type.classList.add('disapear')
+//     }, startTime * 1000)
+// }
+
+
+// ----------------------------- Out of time -----------------------------------------
+function timeout(startTime) {
+
+}
+
+//--------------------------------- get new word ---------------------------------------
+function getNewWord() {
+    var count = 0;
+    // get data array length
+    const wordsdata = words.length
+
+    // get random element in array
+    const random = Math.random() * wordsdata;
+
+    // rounding the randomnumber has earned
+    const index = Math.floor(random);
+
+    // return the final index of array
+    return words[index];
+}
+
+//--------------------------- replace old word has typed -----------------------------
+function replace() {
+    const newWord = getNewWord();
+
+    // replace
+    text.textContent = newWord;
+
+    // reset input
+    type.value = '';
+}
+
+//--------------------------- Listening event on input , on key down -----------------------------
+
+type.addEventListener("input", (e) => {
+    // input value
+    const value = myInput.value;
+
+    // word data
+    var newWord = text.textContent
+
+    var splitString = hide.split('');
+    splitString = splitString.filter(char => {
+        return char.trim() != '';
+    })
+
+    if (splitString[0] == value) {
+        splitString.shift();
+        // hide = splitString.join('');
+    }
+
+    // check if the input value is the same with the word from data
+    if (value === newWord) {
+        e.target.classList.add('correct');
+        setTimeout(() => {
+            e.target.classList.remove('correct');
+            replace();
+        }, 500)
+        allkeyonscreen.forEach(element => {
+            element.classList.remove('changecolor')
+        })
+    }
+})
+
+// Handle change color on keyboard
+type.addEventListener("keydown", (e) => {
+    const value = myInput.value;
+    var newWord = text.textContent
+
+    // Get the key code typed
+    var keyCode = e.keyCode;
+    var keyName = String.fromCharCode(keyCode)
+
+    // check the keycode typed is the same with the key exist on screen
+    checkCorrectKey(allkeyonscreen, newWord, keyName)
+});
+
+
+function checkCorrectKey(keyboard, word, keyName) {
+    keyboard.forEach(e => {
+        if (keyName == e.textContent) {
+
+            if (word.toLowerCase().includes(keyName.toLowerCase())) {
+                // change color if that is the same
+                e.classList.add('changecolor');
+
+                // sound presskey on
+                presskey.play();
+            } else {
+                e.classList.remove('changecolor')
+                e.classList.add('wrong');
+                wrong.play();
+
+                setTimeout(() => {
+                    e.classList.remove('wrong')
+                }, 1000)
+            }
+        }
+    })
+}
+
+
+
---
+++ index.js
@@ -0,0 +1,130 @@
+const rowkey1 = [
+    { name: "Q", order: 1, keycode: 81 },
+    { name: "W", order: 2, keycode: 87 },
+    { name: "E", order: 3, keycode: 69 },
+    { name: "R", order: 4, keycode: 82 },
+    { name: "T", order: 5, keycode: 84 },
+    { name: "Y", order: 6, keycode: 89 },
+    { name: "U", order: 7, keycode: 85 },
+    { name: "I", order: 8, keycode: 73 },
+    { name: "O", order: 9, keycode: 79 },
+    { name: "P", order: 10, keycode: 80 },
+]
+
+const rowkey2 = [
+    { name: "A", order: 1, keycode: 65 },
+    { name: "S", order: 2, keycode: 83 },
+    { name: "D", order: 3, keycode: 68 },
+    { name: "F", order: 4, keycode: 70 },
+    { name: "G", order: 5, keycode: 71 },
+    { name: "H", order: 6, keycode: 72 },
+    { name: "J", order: 7, keycode: 74 },
+    { name: "K", order: 8, keycode: 75 },
+    { name: "L", order: 9, keycode: 76 },
+]
+
+const rowkey3 = [
+    { name: "Z", order: 1, keycode: 90 },
+    { name: "X", order: 2, keycode: 88 },
+    { name: "C", order: 3, keycode: 67 },
+    { name: "V", order: 4, keycode: 86 },
+    { name: "B", order: 5, keycode: 66 },
+    { name: "N", order: 6, keycode: 78 },
+    { name: "M", order: 7, keycode: 77 },
+];
+
+const words = [
+    'Sensor',
+    'Design',
+    'Gaming',
+    'Mobile',
+    'Camera',
+    'Printer',
+    'Source',
+    'Object',
+    'Thread',
+    'Buffer',
+    'Plugin',
+    'Search',
+    'Render',
+    'Engine',
+    'Socket',
+    'Vector',
+    'Filter',
+    'Module',
+    'Script',
+    'Console',
+    'Export',
+    'Gateway',
+    'Daemon',
+    'Project',
+    'Speaker',
+    'Monitor',
+    'Control',
+    'Firmware',
+    'Library',
+    'Display',
+    'Battery',
+    'Database',
+    'Package',
+    'Scanner',
+    'Adapter',
+    'Firewall',
+    'Content',
+    'Network',
+    'Service',
+    'Thay Hoang dep try',
+    'Longlhph31572',
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
