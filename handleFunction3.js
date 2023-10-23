17caefe37444ec287ee530cac3e8cc9d5123e27f
Long LH <luonghoanglong.3008@gmail.com>
2023-10-23 13:10:32 +0700

    check color Letter

--- handleFunction3.js
+++ handleFunction3.js
@@ -9,32 +9,34 @@ const type = document.getElementById("myInput");
 const start = document.getElementById('start');
 const boxtext = document.getElementById('text');
 const text = document.querySelector('#text h3');
-var hide = document.querySelector('#text #hide').value;
-
-var clockCounting = document.querySelector('#clock span');
-
+var lettersFromWord
 //--------------------------- Get all keys exist on screen ---------------------------
 // Select row key on keyboard
 const row1 = document.querySelector('#rowkey1');
 const row2 = document.querySelector('#rowkey2');
 const row3 = document.querySelector('#rowkey3');
 const row4 = document.querySelector('#rowkey4');
+const row5 = document.querySelector('#rowkey5');
 
 // Render key on keyboard
 function renderKey(keyboard, rowkey) {
     keyboard.forEach(e => {
         var key = document.createElement('p');
         key.textContent = e.name;
+        key.id = e.char
         key.classList.add('key');
         key.dataset.keycode = e.keycode
+        key.dataset.shift = e.shift
+        key.dataset.char = e.char
         rowkey.appendChild(key);
     })
 }
 
-// renderKey(rowkey1, row1)
+renderKey(rowkey1, row1)
 renderKey(rowkey2, row2)
 renderKey(rowkey3, row3)
 renderKey(rowkey4, row4)
+renderKey(rowkey5, row5)
 
 const allkeyonscreen = document.querySelectorAll('.key');
 
@@ -44,41 +46,51 @@ start.onclick = () => {
     boxtext.classList.add('appear');
     start.classList.add('disapear');
     type.placeholder = "Type in here...";
+    title.classList.add('disapear')
+    getStart()
 
-    // var interval = setInterval(() => {
-    //     getStart()
-    // }, 1000)
+    var interval = setInterval(() => {
+        title.classList.remove('disapear')
+    }, 1000)
 
-    // setTimeout(() => {
-    //     clearInterval(interval)
-    //     startTime = 5;
-    type.disabled = false;
-    type.focus();
-    replace();
-    //     setNewTime();
-    // }, startTime * 1000)
+    setTimeout(() => {
+        clearInterval(interval)
+        startTime = 5;
+
+        type.disabled = false;
+        type.focus();
+        replace();
+        setNewTime();
+    }, startTime * 1000)
 }
 
-// function getStart() {
-//     startTime--;
-//     clockCounting.textContent = startTime + 's';
-// }
+function getStart() {
+    startTime--;
+    // title.textContent = startTime + 's';
+    title.innerHTML = `
+    <div class="flex items-center justify-center gap-x-2 w-3/12 mx-auto">
+        <img id="logo" width="100px" src="assets/img/down-time.gif" alt="">
+        <p id="clock">${startTime}</p>
+    </div>
+    `
+    // title.classList.remove('disapear')
+}
 
-// function setNewTime() {
-//     startTime = 3;
+function setNewTime() {
+    startTime = 50;
 
-//     var interval = setInterval(() => {
-//         getStart();
-//     }, 1000)
+    var interval = setInterval(() => {
+        getStart();
+    }, 1000)
 
-//     setTimeout(() => {
-//         clearInterval(interval)
-//         startTime = 0;
-//         type.disabled = true;
-//         type.value = 'Your result is ...'
-//         // type.classList.add('disapear')
-//     }, startTime * 1000)
-// }
+    setTimeout(() => {
+        clearInterval(interval)
+        startTime = 0;
+        type.disabled = true;
+        type.value = 'Your result is ...'
+        // type.classList.add('disapear')
+    }, startTime * 1000)
+}
 
 
 // ----------------------------- Out of time -----------------------------------------
@@ -88,7 +100,6 @@ function timeout(startTime) {
 
 //--------------------------------- get new word ---------------------------------------
 function getNewWord() {
-    var count = 0;
     // get data array length
     const wordsdata = words.length
 
@@ -106,38 +117,49 @@ function getNewWord() {
 function replace() {
     const newWord = getNewWord();
 
-    // replace
-    text.textContent = newWord;
+    var split = newWord.split('');
+
+    text.textContent = ''
+
+    split.forEach(e => {
+        const letter = document.createElement('span');
+        letter.textContent = e
+        letter.classList.add('letter')
+        text.appendChild(letter);
+    })
+
+    lettersFromWord = document.querySelectorAll('span')
 
     // reset input
     type.value = '';
 }
 
-
 //--------------------------- Handle Correct/Wrong key on keyboard -----------------------------
 
 function checkCorrectKeyOnBoard(keyboard, input, color, sound) {
     keyboard.forEach(key => {
-        if (input.toUpperCase() === key.textContent) {
+        if (input === key.dataset.char || input === key.dataset.shift) {
             var time
             key.classList.add(color)
             sound.play()
-            color === "changecolor" ? time = 1000 : time = 500 
-            setTimeout(() => { key.classList.remove(color) },time)
+            color === "changecolor" ? time = 1000 : time = 500
+            setTimeout(() => { key.classList.remove(color) }, time)
         }
     })
 }
 
 //--------------------------- Handle Correct/Wrong input String -----------------------------
 
-function checkInputString(inputString,compareString,event) {
+function checkInputString(inputString, compareString, event) {
     var color;
     if (inputString.length === compareString.length) {
         inputString === compareString ? color = "correct" : color = "wrong"
-        event.target.classList.add(color);
+        // event.target.classList.add(color);
+        text.classList.add(color);
         type.disabled = true
         setTimeout(() => {
-            event.target.classList.remove(color);
+            // event.target.classList.remove(color);
+            text.classList.remove(color);
             type.disabled = false
             type.focus()
             replace();
@@ -145,10 +167,21 @@ function checkInputString(inputString,compareString,event) {
     }
 }
 
+//--------------------------- Handle Correct/Wrong letters on New Word -------------------------
+
+function checkCorrectLettersOnWord(letters, index, color) {
+    if (color === 'correctLetter') {
+        letters[index].classList.remove('wrongLetter')
+    }else {
+        letters[index].classList.remove('correctLetter')
+    }
+    letters[index].classList.add(color);
+}
+
 //--------------------------- Listening event on input , on key down -----------------------------
 
 type.addEventListener("input", (event) => {
-    var keySound, keyColor
+    var keySound, keyColor, letterColor
 
     // input value
     var value = myInput.value;
@@ -158,6 +191,7 @@ type.addEventListener("input", (event) => {
 
     // declare counting variable
     indexChar = value.length - 1;
+    console.log(indexChar);
 
     // indexChar from newWord
     var charFromNewWord = newWord.split('')[indexChar]
@@ -165,11 +199,16 @@ type.addEventListener("input", (event) => {
     // indexChar from inputvalue
     var charFromInput = value[indexChar];
 
-    charFromNewWord === charFromInput ? keyColor = "changecolor" : keyColor = "wrongkey"
-    keyColor === "changecolor" ? keySound = presskey : keySound = wrong
-
+    keyColor = charFromNewWord === charFromInput ? "changecolor" : "wrongkey"
+    keySound = keyColor === "changecolor" ? presskey : wrong
     checkCorrectKeyOnBoard(allkeyonscreen, charFromInput, keyColor, keySound)
+
+    // ----
     checkInputString(value, newWord, event)
+
+    // ---
+    letterColor = charFromNewWord === charFromInput ? 'correctLetter' : 'wrongLetter'
+    checkCorrectLettersOnWord(lettersFromWord,indexChar,letterColor)
 })
 
 
